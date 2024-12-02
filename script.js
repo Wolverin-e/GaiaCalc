bced0640dfd1bcf7b443467888af152aa955e04c
Wolverin-e <patel.6@iitj.ac.in>
2024-12-02 11:02:36 +0530

    Updated Calculator to the new version.

--- script.js
+++ script.js
@@ -9,68 +9,248 @@ const {
 
 createApp({
     setup() {
+        const typeSelected = ref(null)
+        const numberOfEmployees = ref('')
+        const aiCreditsSelected = ref(null)
+        const numberOfAdsSelected = ref(null)
 
-        const jobBoardsUsed = ref(0)
-        const jobPostingPerMonth = ref()
-        const avgAppsPerJobPosting = ref()
-        const frequencyOfOverlap = ref('')
-        const monthlySpendOnJobBoards = ref()
+        // Options for Select
+        const numberOfAdsOpts = [1, 25, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 725, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000];
+        const aiCreditsOpts = [0, 10000, 50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
 
-        const baselineOverlapRate = computed(() => {
 
-            const x = jobBoardsUsed.value
-            if (x > 0 && x <= 2) {
-                return 30;
-            } else if (x > 2 && x <= 5) {
-                return 50;
-            } else if (x > 5 && x <= 10) {
-                return 70;
-            } else if (x > 10 && x <= 15) {
-                return 80;
+        const employeesRounded  = computed(() => {
+            // console.log("number=>", numberOfEmployees.value);
+
+            if(typeof numberOfEmployees.value !== "number") return 1000
+
+            if (numberOfEmployees.value < 1000) {
+                return 1000;
+            } else {
+                // Round up to the nearest multiple of 5000
+                return Math.ceil(numberOfEmployees.value / 5000) * 5000;
+            }
+        })
+
+        // Computed Values
+        const monthlyPricingCoreSolution = computed(() => {
+
+            // Sample data for 'Pricing Pilot', 'Pricing Managed', and 'Pricing Self Serve'
+            const pricingManaged = {
+                1000: 2813,
+                5000: 5625,
+                10000: 11250,
+                15000: 22500,
+                20000: 45000,
+                25000: 90000,
+                30000: 180000,
+                35000: 360000,
+                40000: 720000,
+                45000: 1440000,
+                50000: 2880000,
+                55000: 5760000,
+                60000: 11520000,
+                65000: 23040000,
+                70000: 46080000,
+                75000: 92160000,
+                80000: 184320000,
+                85000: 368640000,
+                90000: 737280000,
+                95000: 1474560000,
+                100000: 2949120000,
             }
 
-            return 0;
-        })
-
-        const adjustedOverlapRate = computed(() => {
-            let adjustment
-
-            switch (frequencyOfOverlap.value) {
-                case "Rarely":
-                    adjustment = -5;
-                    break;
-                case "Occasionally":
-                    adjustment = 0;
-                    break;
-                case "Often":
-                    adjustment = 5;
-                    break;
-                case "Very Often":
-                    adjustment = 10;
-                    break;
-                default:
-                    adjustment = 0;
+            const pricingPilot = {
+                1000: 1856,
+                5000: 3713,
+                10000: 7425,
+                15000: 14850,
+                20000: 29700,
+                25000: 59400,
+                30000: 118800,
+                35000: 237600,
+                40000: 475200,
+                45000: 950400,
+                50000: 1900800,
+                55000: 3801600,
+                60000: 7603200,
+                65000: 15206400,
+                70000: 30412800,
+                75000: 60825600,
+                80000: 121651200,
+                85000: 243302400,
+                90000: 486604800,
+                95000: 973209600,
+                100000: 1946419200,
             }
 
-            return baselineOverlapRate.value + adjustment
+            const pricingSelfServe = {
+                1: "FREEMIUM",
+                25: 799,
+                75: 1899,
+                100: 2499,
+                125: 3099,
+                150: 3699,
+                175: 4299,
+                200: 4899,
+                225: 5499,
+                250: 6099,
+                275: 6699,
+                300: 7299,
+                325: 7899,
+                350: 8499,
+                375: 9099,
+                400: 9699,
+                425: 10299,
+                450: 10899,
+                475: 11499,
+                500: 12099,
+                525: 12699,
+                550: 13299,
+                575: 13899,
+                600: 14499,
+                625: 15099,
+                650: 15699,
+                675: 16299,
+                700: 16899,
+                725: 17499,
+                750: 18099,
+                775: 18699,
+                800: 19299,
+                825: 19899,
+                850: 20499,
+                875: 21099,
+                900: 21699,
+                925: 22299,
+                950: 22899,
+                975: 23499,
+                1000: 24099,
+            }      
+
+            function hLookup(value, table) {
+                if (table.hasOwnProperty(value)) {
+                    return table[value];
+                }
+
+                return null;
+            }
+
+
+            if(typeSelected.value === 'Pilot') {
+                return hLookup(employeesRounded.value, pricingPilot);  // Lookup in 'Pricing Pilot'
+            } else if (typeSelected.value === "Managed") {
+                return hLookup(employeesRounded.value, pricingManaged);  // Lookup in 'Pricing Managed'
+            } else if (typeSelected.value === "Self Serve") {
+                return hLookup(numberOfAdsSelected.value, pricingSelfServe);  // Lookup in 'Pricing Self Serve'
+            } else {
+                return null;  // Default value if no match
+            }
         })
 
-        const estimatedMonthlyWastedSpend = computed(() => {
-            return monthlySpendOnJobBoards.value * adjustedOverlapRate.value / 100
+        const monthlyPricingAIExtras = computed(() => {
+            const pricing = {
+                0: 0,
+                10000: 799,
+                50000: 3999,
+                100000: 7999,
+                150000: 11999,
+                200000: 15999,
+                250000: 19999,
+                300000: 23999,
+                350000: 27999,
+                400000: 31999,
+                500000: 39999,
+                600000: 47999,
+                700000: 55999,
+                800000: 63999,
+                900000: 71999,
+                1000000: 79999,
+            }
+            
+            return pricing[aiCreditsSelected.value] ?? ""
         })
 
-        const potentialMonthlySavings = computed(() => {
-            return monthlySpendOnJobBoards.value * (adjustedOverlapRate.value - 20) / 100
+        const quarterlyPricingPilot = computed(() => {
+            return typeSelected.value === "Pilot" && typeof monthlyPricingCoreSolution.value === "number"
+                ? monthlyPricingCoreSolution.value * 3
+                : null
+        })
+
+        const yearlyPricingManaged = computed(() => {
+            return (typeSelected.value === "Managed" || typeSelected.value === "Self Serve") && typeof monthlyPricingCoreSolution.value === "number"
+                ? monthlyPricingCoreSolution.value * 12
+                : null
+        })
+
+        const aiCreditsWithCoreSolution = computed(() => {
+
+            const pricingData = {
+                1: 1000,
+                25: 25000,
+                75: 75000,
+                100: 100000,
+                125: 125000,
+                150: 150000,
+                175: 175000,
+                200: 200000,
+                225: 225000,
+                250: 250000,
+                275: 275000,
+                300: 300000,
+                325: 325000,
+                350: 350000,
+                375: 375000,
+                400: 400000,
+                425: 425000,
+                450: 450000,
+                475: 475000,
+                500: 500000,
+                525: 525000,
+                550: 550000,
+                575: 575000,
+                600: 600000,
+                625: 625000,
+                650: 650000,
+                675: 675000,
+                700: 700000,
+                725: 725000,
+                750: 750000,
+                775: 775000,
+                800: 800000,
+                825: 825000,
+                850: 850000,
+                875: 875000,
+                900: 900000,
+                925: 925000,
+                950: 950000,
+                975: 975000,
+                1000: 1000000,
+            };
+
+            return typeSelected.value === "Self Serve"
+                ? pricingData[numberOfAdsSelected.value] ?? null
+                : "UNLIMITED"
+        })
+
+        const aiCreditsAdditionalPurchased = computed(() => {
+            return aiCreditsSelected.value
+        })
+
+        const totalAICredits = computed(() => {
+            if(typeSelected.value !== "Self Serve"){
+                return "UNLIMITED"
+            }
+            if(typeof aiCreditsWithCoreSolution.value !== "number") return "UNLIMITED"
+            if(typeof aiCreditsAdditionalPurchased.value !== "number") return aiCreditsWithCoreSolution.value
+            return aiCreditsWithCoreSolution.value + aiCreditsAdditionalPurchased.value
         })
 
 
         // Form validation
         const onceSubmittedSuccessfully = ref(false)
-        const jobBoardsUsedSelect = ref(null)
-        const jobPostingPerMonthInput = ref(null)
-        const avgAppsPerJobPostingInput = ref(null)
-        const frequencyOfOverlapSelect = ref(null)
-        const monthlySpendOnJobBoardsInput = ref(null)
+        const typeSelect = ref(null)
+        const numberOfEmployeesInput = ref(null)
+        const numberOfAdsSelect = ref(null)
 
         // For tooltips
         const showTooltipFor = (elem, text = "tooltip", arrowXOffset = -50) => {
@@ -141,11 +321,9 @@ createApp({
         }
 
         const allValuesFilled = computed(() => {
-            if(jobBoardsUsed.value === 0) return false
-            if(!jobPostingPerMonth.value) return false
-            if(!avgAppsPerJobPosting.value) return false
-            if(frequencyOfOverlap.value === '') return false
-            if(!monthlySpendOnJobBoards.value) return false
+            if(!typeSelected.value) return false
+            if(!numberOfEmployees.value) return false
+            if(!numberOfAdsSelected.value) return false
 
             return true
         })
@@ -154,11 +332,9 @@ createApp({
 
             const isEmpty = x => x === undefined || x === ""
 
-            if(jobBoardsUsed.value === 0) return showInvalid(jobBoardsUsedSelect.value, jobBoardsUsedSelect.value.parentElement)
-            if(isEmpty(jobPostingPerMonth.value)) return showInvalid(jobPostingPerMonthInput.value, jobPostingPerMonthInput.value.parentElement)
-            if(isEmpty(avgAppsPerJobPosting.value)) return showInvalid(avgAppsPerJobPostingInput.value, avgAppsPerJobPostingInput.value.parentElement)
-            if(isEmpty(frequencyOfOverlap.value)) return showInvalid(frequencyOfOverlapSelect.value, frequencyOfOverlapSelect.value.parentElement)
-            if(isEmpty(monthlySpendOnJobBoards.value)) return showInvalid(monthlySpendOnJobBoardsInput.value, monthlySpendOnJobBoardsInput.value.parentElement.parentElement)
+            if(typeSelected.value === null) return showInvalid(typeSelect.value, typeSelect.value.parentElement)
+            if(isEmpty(numberOfEmployees.value)) return showInvalid(numberOfEmployeesInput.value, numberOfEmployeesInput.value.parentElement)
+            if(numberOfAdsSelected.value === null) return showInvalid(numberOfAdsSelect.value, numberOfAdsSelect.value.parentElement)
 
             onceSubmittedSuccessfully.value = true
         }
@@ -166,25 +342,28 @@ createApp({
 
         return {
             // State
-            jobBoardsUsed,
-            jobPostingPerMonth,
-            avgAppsPerJobPosting,
-            frequencyOfOverlap,
-            monthlySpendOnJobBoards,
+            typeSelected,
+            numberOfEmployees,
+            aiCreditsSelected,
+            numberOfAdsOpts,
+            aiCreditsOpts,
+            numberOfAdsSelected,
 
             // Derivations
-            baselineOverlapRate,
-            adjustedOverlapRate,
-            estimatedMonthlyWastedSpend,
-            potentialMonthlySavings,
+            employeesRounded,
+            monthlyPricingCoreSolution,
+            monthlyPricingAIExtras,
+            quarterlyPricingPilot,
+            yearlyPricingManaged,
+            aiCreditsWithCoreSolution,
+            aiCreditsAdditionalPurchased,
+            totalAICredits,
 
             // Form validation
             onceSubmittedSuccessfully,
-            jobBoardsUsedSelect,
-            jobPostingPerMonthInput,
-            avgAppsPerJobPostingInput,
-            frequencyOfOverlapSelect,
-            monthlySpendOnJobBoardsInput,
+            typeSelect,
+            numberOfEmployeesInput,
+            numberOfAdsSelect,
             allValuesFilled,
             onSubmit,
         }
